139a5adbd5a42c3578a59482adb6fa6d8e87779f
Sungju Kwon <sungju.kwon@gmail.com>
2017-08-07 00:55:35 -0400

    Displaying LOCKUP delays with the involved tasks

---
+++ lockup.py
@@ -0,0 +1,30 @@
+"""
+ Written by Daniel Sungju Kwon
+"""
+
+from __future__ import print_function
+
+from pykdump.API import *
+
+from LinuxDump import Tasks
+
+import sys
+
+def getKey(rqobj):
+    return rqobj.Timestamp
+
+def lockup():
+    rqlist = Tasks.getRunQueues()
+    rqsorted = sorted(rqlist, key=getKey, reverse=True)
+    now = -1
+    for rq in rqsorted:
+        if (now == -1):
+            now = rq.Timestamp
+
+        print ("CPU %3d: %10.2f sec behind by 0x%x, %s (%d in queue)" %
+               (rq.cpu, (now - rq.Timestamp) / 1000000000,
+                rq.curr, rq.curr.comm, rq.nr_running))
+
+
+if ( __name__ == '__main__'):
+    lockup()
