6fcac56b8d41568a79dde88a4a65398d6174faac
Sungju Kwon <sungju.kwon@gmail.com>
2017-08-07 22:26:08 -0400

    installation script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Installing crash extensions running on PyKdump"
+echo ""
+echo "Please make sure 'PyKdump' is already configured to be loaded"
+echo "during crash utlity starting."
+echo "You can check it by looking 'extend' command in ~/.crashrc"
+echo ""
+echo "If you don't have PyKdump' extension, you can download it from"
+echo "the below link"
+echo ""
+echo "      Python/CRASH API"
+echo "      https://sourceforge.net/projects/pykdump/"
+echo ""
+
+# Set the extention code path in .bash_profile
+echo -n "Setting the extention code path in .bash_profile ..."
+echo '' >> ~/.bash_profile
+echo "export PYKDUMPPATH=$PWD" >> ~/.bash_profile
+echo " [DONE]"
+
+# Make it load the registeration code during crash start
+echo -n "Making it load the registeration code during crash start ..."
+echo '' >> ~/.crashrc
+echo "epython $PWD/regext.py" >> ~/.crashrc
+echo " [DONE]"
+
+
+echo ""
+echo "All Done"
