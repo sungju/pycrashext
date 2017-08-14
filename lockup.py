aaf6678dbebc395ce420a78bc16dee306ce4f36c
Sungju Kwon <sungju.kwon@gmail.com>
2017-08-13 21:17:47 -0400

    reverse printing in lockup command

--- lockup.py
+++ lockup.py
@@ -14,18 +14,29 @@ import sys
 def getKey(rqobj):
     return rqobj.Timestamp
 
-def lockup():
+def lockup_display(reverse_sort):
     rqlist = Tasks.getRunQueues()
-    rqsorted = sorted(rqlist, key=getKey, reverse=True)
-    now = -1
-    for rq in rqsorted:
-        if (now == -1):
-            now = rq.Timestamp
+    rqsorted = sorted(rqlist, key=getKey, reverse=reverse_sort)
+    if (reverse_sort):
+        now = rqsorted[0].Timestamp
+    else:
+        now = rqsorted[-1].Timestamp
 
+    for rq in rqsorted:
         print ("CPU %3d: %10.2f sec behind by 0x%x, %s (%d in queue)" %
                (rq.cpu, (now - rq.Timestamp) / 1000000000,
                 rq.curr, rq.curr.comm, rq.nr_running))
 
+
+def lockup():
+    op = OptionParser()
+    op.add_option("-r", dest="reverse_sort", default=0,
+                  action="store_true",
+                  help="show longest holder at top")
+
+    (o, args) = op.parse_args()
+
+    lockup_display(not o.reverse_sort)
 
 if ( __name__ == '__main__'):
     lockup()
--- regext.py
+++ regext.py
@@ -38,6 +38,8 @@ if (symbol_exists('balloon')):
 
 help = '''
 CPU lock check
+
+-r     - Show longest holder at top
 '''
 
 rprog("lockup", "LOCKUP check",
