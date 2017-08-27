cf56ac1fe0a80d336b7577de47e2836633377cce
Sungju Kwon <sungju.kwon@gmail.com>
2017-08-26 21:04:48 -0400

    IPMI information

---
+++ ipmi.py
@@ -0,0 +1,73 @@
+"""
+ Written by Daniel Sungju Kwon
+"""
+
+from __future__ import print_function
+from __future__ import division
+
+from pykdump.API import *
+
+import sys
+
+def si_state_str(state):
+    return {
+        0: "SI_NORMAL",
+        1: "SI_GETTING_FLAGS",
+        2: "SI_GETTING_EVENTS",
+        3: "SI_CLEARING_FLAGS",
+        4: "SI_GETTING_MESSAGES",
+        5: "SI_CHECKING_ENABLES",
+        6: "SI_SETTING_ENABLES",
+    }[state]
+
+def irq_status(disabled):
+    if (disabled):
+        return "DISABLED"
+    else:
+        return "ENABLED"
+
+def show_smi_list(show_details):
+    pa = readSymbol("smi_infos")
+    if (pa == 0):
+        print ("SMI Info does not exist")
+        return
+
+    print ("%-18s %-18s %-18s %-18s" % ("smi_info",
+                                        "ipmi_smi_t",
+                                        "struct si_sm_data",
+                                        "struct si_sm_handlers"))
+    for smi_info  in readSUListFromHead(pa,
+                                        'link',
+                                        'struct smi_info'):
+        print ("0x%x 0x%x 0x%x 0x%x (%s)" % (long(smi_info),
+                                             smi_info.intf,
+                                             smi_info.si_sm,
+                                             smi_info.handlers,
+                                             addr2sym(smi_info.handlers)))
+        if (show_details):
+            print ("%20s : 0x%x" % ("curr_msg", smi_info.curr_msg))
+            print ("%20s : 0x%x" % ("waiting_msg", smi_info.waiting_msg))
+            print ("%20s : %s" % ("state", si_state_str(smi_info.si_state)))
+            print ("%20s : %d" % ("IRQ", smi_info.irq))
+            su = readSU("struct task_struct", smi_info.thread);
+            print ("%20s : %s (0x%x)" % ("kernel thread",
+                                         su.comm,
+                                         smi_info.thread))
+
+
+def ipmi():
+    op = OptionParser()
+    op.add_option("--smi_list", dest="smi_list", default=0,
+                  action="store_true",
+                  help="Show info list")
+    op.add_option("--details", dest="show_details", default=0,
+                  action="store_true",
+                  help="Show detailed information")
+
+    (o, args) = op.parse_args()
+
+    if (o.smi_list):
+        show_smi_list(o.show_details)
+
+if ( __name__ == '__main__'):
+    ipmi()
