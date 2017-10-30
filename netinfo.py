8097d9df6cceec7e671d44a185381b7556acab36
Sungju Kwon <sungju.kwon@gmail.com>
2017-10-29 21:32:26 -0400

    network driver information provided

---
+++ netinfo.py
@@ -0,0 +1,73 @@
+"""
+ Written by Daniel Sungju Kwon
+"""
+
+from __future__ import print_function
+from __future__ import division
+
+from pykdump.API import *
+from LinuxDump.inet import *
+from LinuxDump.inet import proto, netdevice
+
+import sys
+
+
+def show_network_interfaces(options):
+    for dev in netdevice.dev_base_list():
+        dev_addr = "Not available"
+        if (dev.dev_addr != 0):
+            dev_addr = ("%x:%x:%x:%x:%x:%x" %
+                        (dev.dev_addr[0], dev.dev_addr[1],
+                         dev.dev_addr[2], dev.dev_addr[3],
+                         dev.dev_addr[4], dev.dev_addr[5]))
+
+
+        netdev_ops = dev.netdev_ops
+        netdev_ops_str = addr2sym(netdev_ops)
+        driver_module = ""
+        crashout = exec_crash_command("sym %s" %
+                                      (netdev_ops_str))
+        driver_module = crashout.split()[-1]
+        if (driver_module[0] != "["):
+            driver_module = "<built-in>"
+
+        print ("0x%016x : %s (%s) managed by %s" %
+               (dev, dev.name, dev_addr, driver_module))
+        if (options.network_details):
+            print ("\tnetdev_ops = %s (0x%x)" %
+                   (netdev_ops_str, netdev_ops))
+
+            master_name = ""
+            if (dev.master != 0):
+                master_name = dev.master.name
+
+            print("\tMTU: %d, Master = <%s>" %
+                  (dev.mtu, master_name))
+
+            print("\tIRQ: %d, num_tx_queues = %d, real_num_tx_queues = %d" %
+                  (dev.irq, dev.num_tx_queues, dev.real_num_tx_queues))
+
+
+
+def netinfo():
+    op = OptionParser()
+    op.add_option("--interface", dest="show_interface", default=0,
+                  action="store_true",
+                  help="Show network interfaces")
+
+    op.add_option("--details", dest="network_details", default=0,
+                  action="store_true",
+                  help="Show network details")
+
+    (o, args) = op.parse_args()
+
+    if (o.show_interface):
+        show_network_interfaces(o)
+        system.exit(0)
+
+
+    show_network_interfaces(o)
+
+
+if ( __name__ == '__main__'):
+    netinfo()
