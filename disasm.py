868a3e1345f2348d6466dfce5727bf2f647537cc
Sungju Kwon <sungju.kwon@gmail.com>
2018-06-24 03:09:34 -0400

    Server can be set via environment variable
    CRASHEXT_SERVER is used to set the server

--- disasm.py
+++ disasm.py
@@ -1,12 +1,14 @@
+from __future__ import print_function
 import sys
 import base64
 import requests as r
+import os
 
 
 orig_asm = "".join(sys.stdin.readlines()).encode()
 encoded_asm = base64.b64encode(orig_asm)
 
-encode_url = 'http://minilab.usersys.redhat.com:5000/api/disasm'
+encode_url = os.environ['CRASHEXT_SERVER'] + '/api/disasm'
 data = {"asm_str" : encoded_asm}
 res = r.post(encode_url, data = data).text
-print (res)
+print (res, end='')
