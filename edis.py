a2a71d41475b561310f6d6742bb963e8650175f1
Sungju Kwon <sungju.kwon@gmail.com>
2018-07-02 01:40:20 -0400

    Handling command execution properly.

--- edis.py
+++ edis.py
@@ -12,6 +12,27 @@ from LinuxDump import Tasks
 import sys
 import operator
 import os
+from os.path import expanduser
+import time
+
+
+def run_gdb_command(command):
+    """exec_gdb_command() is failing to capture the output
+    if the command is with '!' which is important to execute
+    shell commands. Below will capture it properly."""
+    temp_name = expanduser("~") + "/" + time.strftime("%Y%m%d-%H%M%S-pycrashext-edis")
+    command = command + " > " + temp_name
+    exec_gdb_command(command)
+    lines = ""
+    if os.path.exists(temp_name):
+        with open(temp_name, 'r') as f:
+            try:
+                lines = "".join(f.readlines())
+            except:
+                lines = "Failed to read " + temp_name
+
+    os.remove(temp_name)
+    return lines
 
 
 def get_kernel_version():
@@ -73,7 +94,7 @@ def disasm(ins_addr, o, cmd_path_list):
         print (disasm_str)
         return
 
-    result_str = exec_gdb_command("!echo '%s' | python %s %s" % \
+    result_str = run_gdb_command("!echo '%s' | python %s %s" % \
                                   (disasm_str, disasm_path, cmd_options))
 
     print (result_str)
