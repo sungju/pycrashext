0985495e51a3ddbc1af6bc5e51c4c84f3fc731d1
Sungju Kwon <sungju.kwon@gmail.com>
2018-07-21 16:30:16 +1000

    Makre sure required commands are installed

--- remoteapi/run_standalone.sh
+++ remoteapi/run_standalone.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-cd web
-virtualenv .
-source bin/activate
-pip install -r requirements.txt
+usage() {
+	echo 
+	echo "To run this server as a standalone, you need to have"
+	echo "the below commands installed."
+	echo
+	echo "python"
+	echo "virtualenv"
+	echo "pip"
+	echo
+}
 
-python app.py
+cd web
+virtualenv .  >/dev/null 2>&1 || { usage; exit -1; }
+source bin/activate
+pip install -r requirements.txt  >/dev/null 2>&1 || { usage; exit -2; }
+
+python app.py || { usage; exit -3; }
