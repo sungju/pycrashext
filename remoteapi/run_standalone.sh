32f485e55793473d5b36e54aad3ad5c9f1229188
Sungju Kwon <sungju.kwon@gmail.com>
2018-07-21 16:53:53 +1000

    check the source env variable setting

--- remoteapi/run_standalone.sh
+++ remoteapi/run_standalone.sh
@@ -11,9 +11,28 @@ usage() {
 	echo
 }
 
-cd web
-virtualenv .  >/dev/null 2>&1 || { usage; exit -1; }
-source bin/activate
-pip install -r requirements.txt  >/dev/null 2>&1 || { usage; exit -2; }
+if [ -z $RHEL_SOURCE_DIR ] || [ $RHEL_SOURCE_DIR == "" ]; then
+	echo "RHEL_SOURCE_DIR bash variable should be configured"
+	echo "and should point to the source directory."
+	echo
+	echo "  example)  export RHEL_SOURCE_DIR='/home/dkwon/source/'"
+	echo
+	echo "The source tree should be something like below."
+	echo
+	echo "<your_source_dir> -+-- rhel5"
+	echo "                   +-- rhel6"
+        echo "                   ..."
+        echo "                   +-- fedora"
+	echo
+	echo "The directory doesn't need to have all source repositories."
+	echo "It only needs to have the directories you are going to use."
+	echo
+	exit -1
+fi
 
-python app.py || { usage; exit -3; }
+cd web
+virtualenv .  >/dev/null 2>&1 || { usage; exit -2; }
+source bin/activate
+pip install -r requirements.txt  >/dev/null 2>&1 || { usage; exit -3; }
+
+python app.py || { usage; exit -4; }
