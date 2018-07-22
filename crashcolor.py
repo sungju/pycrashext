0691c1d03e16df4515fb5f0eb90b9b7bb98713cc
Sungju Kwon <sungju.kwon@gmail.com>
2018-07-22 18:24:32 -0400

    fixing a delay caused in color code

--- crashcolor.py
+++ crashcolor.py
@@ -41,46 +41,49 @@ MAX_MODE = INVERT
 COLOR_MASK = 0x00ff
 MODE_MASK = 0xff00
 
+
+color_list = {
+    BLACK : "\\033[30m",
+    RED : "\\033[31m",
+    GREEN : "\\033[32m",
+    YELLOW : "\\033[33m",
+    BLUE : "\\033[34m",
+    MAGENTA : "\\033[35m",
+    CYAN : "\\033[36m",
+    LIGHTGRAY : "\\033[37m",
+    DARKGRAY : "\\033[38m",
+    LIGHTRED : "\\033[91m",
+    LIGHTGREEN : "\\033[92m",
+    LIGHTYELLOW : "\\033[93m",
+    LIGHTBLUE : "\\033[94m",
+    LIGHTMAGENTA : "\\033[95m",
+    LIGHTCYAN : "\\033[96m",
+    WHITE : "\\033[97m",
+    RESET : "\\033[0m",
+    BOLD : "\\033[1m",
+    BLINK : "\\033[5m",
+    UNDERLINE : "\\033[4m",
+    INVERT : "\\033[7m",
+}
+
 def set_color(color_mix):
     if not sys.stdout.isatty():
         return
 
-    color_list = {
-        BLACK : "\\033[30m",
-        RED : "\\033[31m",
-        GREEN : "\\033[32m",
-        YELLOW : "\\033[33m",
-        BLUE : "\\033[34m",
-        MAGENTA : "\\033[35m",
-        CYAN : "\\033[36m",
-        LIGHTGRAY : "\\033[37m",
-        DARKGRAY : "\\033[38m",
-        LIGHTRED : "\\033[91m",
-        LIGHTGREEN : "\\033[92m",
-        LIGHTYELLOW : "\\033[93m",
-        LIGHTBLUE : "\\033[94m",
-        LIGHTMAGENTA : "\\033[95m",
-        LIGHTCYAN : "\\033[96m",
-        WHITE : "\\033[97m",
-        RESET : "\\033[0m",
-        BOLD : "\\033[1m",
-        BLINK : "\\033[5m",
-        UNDERLINE : "\\033[4m",
-        INVERT : "\\033[7m",
-    }
-
     color = color_mix & COLOR_MASK
     mode = color_mix & MODE_MASK
 
+    color_ansi_code = ""
     # Set text color
     if color in color_list:
-        result_str = exec_crash_command("gdb echo " + color_list[color])
-        print (result_str, end='')
+        color_ansi_code = color_list[color]
 
     # Set text mode
-    for cur_mode in range(MIN_MODE, MAX_MODE):
+    for cur_mode in range(MIN_MODE, MAX_MODE, MIN_MODE):
         cur_color = mode & cur_mode
         if cur_color in color_list:
-            result_str = exec_crash_command("gdb echo " +
-                                            color_list[cur_color])
-            print (result_str, end='')
+            color_ansi_code = color_ansi_code + color_list[cur_color]
+
+    if len(color_ansi_code) > 0:
+        result_str = exec_crash_command("gdb echo " + color_ansi_code)
+        print (result_str, end='')
