84448d0f1192df4d482401693e0dbb224df1006e
Sungju Kwon <sungju.kwon@gmail.com>
2018-07-22 22:19:33 -0400

    for black background terminal

--- pstree.py
+++ pstree.py
@@ -62,7 +62,7 @@ TASK_DEAD = 64
 
 task_state_color_list = {
     TASK_RUNNING : crashcolor.BLUE,
-    TASK_INTERRUPTIBLE : crashcolor.BLACK,
+    TASK_INTERRUPTIBLE : crashcolor.RESET,
     TASK_UNINTERRUPTIBLE : crashcolor.RED,
     __TASK_STOPPED : crashcolor.CYAN,
     __TASK_TRACED : crashcolor.MAGENTA,
