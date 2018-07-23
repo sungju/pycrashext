2f80673d05d56f397a9b31f52f9e6618d62315aa
Sungju Kwon <sungju.kwon@gmail.com>
2018-07-23 02:12:23 -0400

    Main way to check 3rd party modules is module.taints

--- modinfo.py
+++ modinfo.py
@@ -41,6 +41,9 @@ def is_our_module(module_addr):
     if module == None:
         return False
 
+    if member_offset("struct module", "taints") > -1:
+        return module.taints == 0
+
     if member_offset("struct module", "sig_ok") > -1:
         return module.sig_ok != 0
 
