672ec2eceff1723af17cb46a15a9a3feb93ca6f9
Daniel Sungju Kwon <sungju.kwon@gmail.com>
2018-09-13 11:32:26 +1000

    Direct usage of insights-core
    As insights-core's new feature to redirect output, now we don't
    need to call standalone application for insights usage.

--- remoteapi/web/plugins/insights.py
+++ remoteapi/web/plugins/insights.py
@@ -8,8 +8,56 @@ from flask import Flask
 from flask import request
 import re
 import os
+import sys
 import base64
 import subprocess
+import json
+import six
+
+
+### Insights related modules
+### ------------------------
+from insights import dr
+#from insights.formats.text import HumanReadableFormat as Formatter
+from insights.formats._json import JsonFormat as Formatter
+
+from insights.specs import Specs
+from insights.tests import context_wrap
+### ------------------------
+
+def insights_call(data_str):
+    decoded_str = base64.b64decode(data_str)
+    data_dict = json.loads(decoded_str)
+
+    try:
+        if "INSIGHTS_RULES" in os.environ:
+            rules_list = os.environ["INSIGHTS_RULES"].split(":")
+            for rule_path in rules_list:
+                if rule_path not in sys.path:
+                    sys.path.append(rule_path)
+                dr.load_components(os.path.basename(rule_path))
+    except Exception as e:
+        return str(e)
+
+    try:
+        dr.load_components("insights.specs.default")
+        broker = dr.Broker()
+        broker[Specs.hostname] = context_wrap(data_dict["hostname"])
+        broker[Specs.uname] = context_wrap(data_dict["uname"])
+        broker[Specs.dmesg] = context_wrap(data_dict["dmesg"])
+        broker[Specs.messages] = context_wrap(data_dict["dmesg"])
+        broker[Specs.sysctl] = context_wrap(data_dict["sysctl"])
+        broker[Specs.meminfo] = context_wrap(data_dict["meminfo"])
+
+        output = six.StringIO()
+        with Formatter(broker, stream=output):
+            dr.run(broker=broker)
+
+        output.seek(0)
+        data = output.read()
+        return data
+    except Exception as e:
+        return str(e)
 
 
 def add_plugin_rule(app):
@@ -29,14 +77,4 @@ def get_data_from_post(entity):
 
 
 def insights():
-    try:
-        process = subprocess.Popen('python insights_call.py',
-                                   shell=True,
-                                   stdin=subprocess.PIPE,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT)
-        result = process.communicate(input=str.encode(get_data_from_post("data")))
-        return result
-    except Exception as e:
-        return str(e)
-
+    return insights_call(get_data_from_post("data"))
