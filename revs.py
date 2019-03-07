afdd7466f9a44b8ceb0e2999a4b153b538a031db
Sungju Kwon <sungju.kwon@gmail.com>
2019-03-08 09:45:48 +1000

    Change the data structure to embrace other arch

--- revs.py
+++ revs.py
@@ -49,6 +49,14 @@ def show_registers():
         show_register_details("ppc")
 
 
+def is_arch_match(arch, arch_list_str):
+    arch_list = arch_list_str.split()
+    for arch_entry in arch_list:
+        if arch.startswith(arch_entry):
+            return True
+    return False
+
+
 def read_database():
     file_lines = []
     try:
@@ -59,37 +67,57 @@ def read_database():
             if os.path.exists(path + "/revs.data"):
                 source_file = path + "/revs.data"
                 break
-        inst_set = ["REGISTERS", "INSTRUCTION"]
-        idx = 0
+
+        arch = sys_info.machine
+        arch_total_msg = ""
         with open(source_file, 'r') as f:
             for line in f:
                 words = line.split(":")
-                if words[0] in inst_set:
-                    if words[0] == "REGISTERS":
-                        arch_list = words[1] # don't split yet
-                        register_details = ""
-                        idx = idx + 1
-                        for detail_line in f:
-                            idx = idx + 1
-                            if detail_line == "END_" + line:
-                                arch_register_list[arch_list] = register_details
-                                break
-                            register_details = register_details + detail_line
-                    elif words[0] == "INSTRUCTION":
-                        inst_list = words[1] # don't split yet
-                        inst_details = ""
-                        idx = idx + 1
-                        for detail_line in f:
-                            idx = idx + 1
-                            if detail_line == "END_" + line:
-                                instruction_list[inst_list] = inst_details
-                                break
-                            inst_details = inst_details + detail_line
-                        idx = idx + 1
-                else:
-                    idx = idx + 1
+                if words[0] == "ARCHITECTURE":
+                    arch_list = words[1]
+                    if is_arch_match(arch, arch_list) == False:
+                        continue
 
-    except:
+                    for detail_line in f:
+                        if detail_line == "END_" + line:
+                            break
+                        arch_total_msg = arch_total_msg + detail_line
+
+
+        inst_set = ["REGISTERS", "INSTRUCTION"]
+        cur_mode = 0
+        cur_data_line = ""
+        arch_list = ""
+        total_line = ""
+
+        for line in arch_total_msg.splitlines():
+            words = line.split(":")
+            if words[0] in inst_set:
+                cur_data_line = line
+                if words[0] == "REGISTERS":
+                    arch_list = words[1] # don't split yet
+                    total_line = ""
+                    cur_mode = 1
+                elif words[0] == "INSTRUCTION":
+                    inst_list = words[1] # don't split yet
+                    total_line = ""
+                    cur_mode = 2
+            elif line == "END_" + cur_data_line:
+                if cur_mode == 1: # register
+                    arch_register_list[arch_list] = total_line
+                elif cur_mode == 2: # instruction
+                    instruction_list[inst_list] =  total_line
+
+                cur_data_line = ""
+                arch_list = ""
+                inst_list = ""
+                total_line = ""
+                cur_mode = 0
+            else:
+                total_line = total_line + line + "\n"
+
+    except Exception as e:
+        print(e)
         print("Failed to read file %s" % (source_file))
         return
 
