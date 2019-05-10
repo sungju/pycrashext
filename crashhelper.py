f26192b0805e62529140e36025a919268ac41182
Daniel Sungju Kwon <sungju.kwon@gmail.com>
2019-05-10 13:52:56 +1000

    Adding copyright

--- crashhelper.py
+++ crashhelper.py
@@ -1,10 +1,24 @@
-"""
- Written by Daniel Sungju Kwon
-"""
-
-from __future__ import print_function
-from __future__ import division
-
+#!/usr/bin/env/python
+# --------------------------------------------------------------------
+# (C) Copyright 2018-2019 Red Hat, Inc.
+#
+# Author: Daniel Sungju Kwon <dkwon@redhat.com>
+#
+# This is providing helper functions to run shell commands
+#
+#
+# Contributors:
+# --------------------------------------------------------------------
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 from pykdump.API import *
 from os.path import expanduser
 import os
