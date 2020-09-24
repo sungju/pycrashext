97e3d46bbc448e8a647215fbadec430e10c79ca0
Sungju Kwon <sungju.kwon@gmail.com>
2020-09-24 19:31:56 -0400

    Shows sched_features

--- schedinfo.py
+++ schedinfo.py
@@ -10,6 +10,7 @@ from pykdump.API import *
 from LinuxDump import Tasks
 
 import sys
+import crashcolor
 
 def show_sched_details(sched_class):
     result = exec_crash_command("sched_class %s" %
@@ -33,6 +34,18 @@ def show_class_list(show_details):
             show_sched_details(sched_class)
 
 
+def show_sched_features(options):
+    sched_feat_keys = readSymbol("sched_feat_keys")
+    sched_feat_names = readSymbol("sched_feat_names")
+    enable_str = crashcolor.get_color(crashcolor.BLUE) + "enabled"
+    disable_str = crashcolor.get_color(crashcolor.RED) + "disabled"
+    for i in range(0, len(sched_feat_keys)):
+        print("%-20s : %s" % (sched_feat_names[i],
+                           enable_str if sched_feat_keys[i].enabled.counter > 0 else
+                              disable_str))
+        crashcolor.set_color(crashcolor.RESET)
+
+
 def schedinfo():
     op = OptionParser()
     op.add_option("-c", "--classes", dest="sched_classes", default=0,
@@ -41,11 +54,18 @@ def schedinfo():
     op.add_option("-d", "--details", dest="show_details", default=0,
                   action="store_true",
                   help="Show details")
+    op.add_option("-f", "--sched_features", dest="sched_features", default=0,
+                  action="store_true",
+                  help="Show /sys/kernel/debug/sched_features")
 
     (o, args) = op.parse_args()
 
     if (o.sched_classes):
         show_class_list(o.show_details)
+
+    if (o.sched_features):
+        show_sched_features(o)
+
 
 if ( __name__ == '__main__'):
     schedinfo()
