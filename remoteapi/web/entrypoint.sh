0981bdb622296b29f8d32fa2f9d3a2dadec0e3d8
Sungju Kwon <sungju.kwon@gmail.com>
2022-03-24 22:40:24 -0400

    Remove insights

--- remoteapi/web/entrypoint.sh
+++ remoteapi/web/entrypoint.sh
@@ -3,41 +3,16 @@
 rm -rf bin/
 
 python3 -m venv .
-if [[ "$OSTYPE" == "msys" ]]; then
+MYOS="$OSTYPE"
+if ( test "$MYOS" = "msys" ); then
 	source Scripts/activate.bat
 else
 	. bin/activate
 fi
 
-export PYTHONPATH="/usr/lib/python3/dist-packages:/usr/local/lib/python3.6/dist-packages:/usr/lib/python3.6/site-packages:$PWD/insights-core"
-
 pip3 install wheel
 python3 setup.py bdist_wheel 
-
-pip3 install -e insights-core 
-
-# Install additional rules
-if [ ! -z "$INSIGHTS_RULES" ]; then
-    IFS=':' read -r -a rules_list <<< "$INSIGHTS_RULES"
-    for rule_path in "${rules_list[@]}"
-    do
-	rule_base_name=$(basename "$rule_path")
-        ln -s "$rule_path" "$rule_base_name"
-	pip3 install -e "$rule_base_name"
-    done
-fi
 
 pip3 install -r requirements.txt
 
 python3 app.py
-
-
-# Delete additional rules
-if [ ! -z "$INSIGHTS_RULES" ]; then
-    IFS=':' read -r -a rules_list <<< "$INSIGHTS_RULES"
-    for rule_path in "${rules_list[@]}"
-    do
-	rule_base_name=$(basename "$rule_path")
-        rm "$rule_base_name"
-    done
-fi
