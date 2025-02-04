e27dc3158bde3a0b0efe73bec8562ec871cb8dcd
Sungju Kwon <sungju.kwon@gmail.com>
2025-02-04 14:51:51 +1000

    Add 'ai' command in server side

---
+++ remoteapi/web/plugins/ai_analyse.py
@@ -0,0 +1,43 @@
+"""
+Written by Daniel Sungju Kwon
+
+It gets some help from specified AI engine for the remote data
+"""
+from flask import Flask
+from flask import request
+import re
+import os
+import base64
+import subprocess
+
+
+def add_plugin_rule(app):
+    app.add_url_rule('/api/ai', 'ai', ai_analyse, methods=['POST'])
+
+
+AI_CMD='ollama'
+AI_RUN='run'
+AI_MODEL='llama3.2'
+
+def disasm():
+    # First line can be used to identify kernel version
+    try:
+        query_str = request.form["query_str"]
+    except:
+        return 'error getting query data'
+
+    try:
+        query_str = base64.b64decode(query_str).decode("utf-8")
+    except:
+        return 'error found in decoding base64'
+
+    result_str = ""
+    try:
+        result = subprocess.run([AI_CMD, AI_RUN, AI_MODEL, query_str],
+                                capture_output=True)
+        result_str = result.stdout.decode()
+    except:
+        result_str = result.stderr.decode()
+
+
+    return result_str.rstrip()
